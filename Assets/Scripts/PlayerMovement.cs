f1d25de0881a30702156ead6d676810368817596
D-Doge <tobias.niedermaier@googlemail.com>
2022-03-30 22:01:55 +0200

    CleanUp PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float AirMoveSpeed = 10;
+    [Header("GroundMovement")]
     public float GroundMoveSpeed = 10;
-    public float AirDrag = 1;
     public float GroundDrag = 2;
+
+    [Header("AirMovement")]
+    public float AirMoveSpeed = 10;
+    public float AirDrag = 1;
+
+    [Header("JumpAndGlide")]
     public float JumpForce = 10;
-    public float playerHeight = 2;
     public float glideGravityScale = 0.5f;
+
+    [Header("GroundDedection")]
     public Transform topLeft;
     public Transform bottomRight;
-
     public LayerMask groundLayer;
+
     private Vector2 moveDirection;
     private bool isGrounded;
     private Rigidbody2D playerBody;
     private int jumpCounter = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +36,23 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         this.moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+
+
+        this.jump();
+    }
+
+    // Override Fixed Update
+    private void FixedUpdate()
+    {
+        this.isGrounded = Physics2D.OverlapArea(this.topLeft.position, this.bottomRight.position, this.groundLayer);
+
+        this.setDrag();
+        this.glide();
+        this.move();
+    }
+
+    private void setDrag()
+    {
         if (this.isGrounded)
         {
             this.playerBody.drag = this.GroundDrag;
@@ -38,22 +62,25 @@ public class PlayerMovement : MonoBehaviour
         {
             this.playerBody.drag = this.AirDrag;
         }
+    }
+
+    private void jump()
+    {
         if (Input.GetButtonDown("Jump"))
         {
             if(this.isGrounded)
             {
                 this.playerBody.AddForce(new Vector2(0, this.JumpForce), ForceMode2D.Impulse);
-            } else if (this.jumpCounter > 0) {
+            } 
+            else if (this.jumpCounter > 0) {
                 this.playerBody.AddForce(new Vector2(0, this.JumpForce), ForceMode2D.Impulse);
                 this.jumpCounter--;
             }
         }
     }
 
-    // Override Fixed Update
-    private void FixedUpdate()
+    private void move()
     {
-        this.isGrounded = Physics2D.OverlapArea(this.topLeft.position, this.bottomRight.position, this.groundLayer);
         if(this.isGrounded)
         {
             this.playerBody.AddForce(this.moveDirection * this.GroundMoveSpeed, ForceMode2D.Force);
@@ -62,15 +89,17 @@ public class PlayerMovement : MonoBehaviour
         {
             this.playerBody.AddForce(this.moveDirection * this.AirMoveSpeed, ForceMode2D.Force);
         }
+    }
 
+    private void glide()
+    {
         if (Input.GetButton("Jump"))
         {
-            this.playerBody.gravityScale = 0.5f;
+            this.playerBody.gravityScale = this.glideGravityScale;
         }
         else
         {
             this.playerBody.gravityScale = 1f;
         }
-
     }
 }
