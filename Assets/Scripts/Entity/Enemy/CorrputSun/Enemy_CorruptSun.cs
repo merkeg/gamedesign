97c63b193407285471fde2b8dfac46af641f9007
D-Doge <tobias.niedermaier@googlemail.com>
2022-04-16 22:32:56 +0200

    BOOM

---
+++ Assets/Scripts/Entity/Enemy/CorrputSun/Enemy_CorruptSun.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_CorruptSun : MonoBehaviour
+{
+    public GameObject explosion;
+    private bool idontfuckingknwoanamenameityourselfyoufuckingidiot = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if(this.idontfuckingknwoanamenameityourselfyoufuckingidiot)
+            {
+                Debug.Log("Floppa");
+                Invoke("Explode", 2);
+                this.idontfuckingknwoanamenameityourselfyoufuckingidiot = false;
+            }
+        }
+    }
+
+    private void Explode()
+    {
+        Instantiate(this.explosion, this.transform.position, transform.rotation);
+        GameObject.Destroy(this.gameObject);
+    }
+}
