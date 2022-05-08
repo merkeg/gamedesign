f2bfe25c2220d5a92b04e60bfd9a4c22e929a40c
D-Doge <tobias.niedermaier@googlemail.com>
2022-05-08 22:47:58 +0200

    Cam now zoomes in and out

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -6,9 +6,9 @@ public class CheckPoint : MonoBehaviour
 {
     private VignetteInterface vignette;
     private bool playerOnCheckpoint = false;
-
     public float zoomOutSize = 20;
-    private float defaultSize = 17;
+
+    private CamController camController;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +18,8 @@ public class CheckPoint : MonoBehaviour
             GameObject.Find("Player").transform.position = (Vector3)checkpoint;
         }
         this.vignette = FindObjectOfType<VignetteInterface>();
+
+        this.camController = GameObject.Find("Main Camera").GetComponent<CamController>();
     }
 
     // Update is called once per frame
@@ -36,7 +38,7 @@ public class CheckPoint : MonoBehaviour
         {
             GameManager.Instance.CheckpointReached(this.transform.position);
             this.playerOnCheckpoint = true;
-            Camera.main.orthographicSize = this.zoomOutSize;
+            this.camController.trySetDiseredZoom(this.zoomOutSize, 1, 1);
         }
     }
 
@@ -45,7 +47,7 @@ public class CheckPoint : MonoBehaviour
         if(other.gameObject.tag == "Player")
         {
             this.playerOnCheckpoint = false;
-            Camera.main.orthographicSize = this.defaultSize;
+            this.camController.trySetDiseredZoom(CamController.defaulZoom, 1, 0);
         }
     }
 }
---
+++ Assets/Scripts/Entity/Player/CamController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public Transform player;
+
+    public float minZoom = 12;
+    public float darkZoomBuffer = 3;
+    public float darkZoomScale = 0.5f;
+
+    public static float defaulZoom = 17;
+    private float desiredZoom = 17;
+
+    private VignetteInterface vignette;
+
+    private int currentPrio = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.vignette = FindObjectOfType<VignetteInterface>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.player.position.x, this.player.position.y, this.transform.position.z);
+
+        
+
+        this.zoom();        
+    }
+
+    private void zoom()
+    {
+        this.trySetDiseredZoom(defaulZoom, 0, 0);
+
+        float darkZoom = (1 - this.vignette.GetIntensity()) * defaulZoom;
+        darkZoom += this.darkZoomBuffer;
+        if(darkZoom < defaulZoom)
+        {
+            if(darkZoom < this.minZoom)
+            {
+                darkZoom = this.minZoom;
+            }
+            this.trySetDiseredZoom(darkZoom, 0, 0);
+        }
+
+        Camera.main.orthographicSize += (desiredZoom - Camera.main.orthographicSize) * Time.deltaTime;
+    }
+
+    // prio is checked, to see if we can override the zoom. prioAfter is the new value the prio will take after setting the desired prio (So we can actually bring it down)
+    public void trySetDiseredZoom(float desiredZoom, int prio, int prioAfter)
+    {
+        if(prio >= this.currentPrio)
+        {
+            this.desiredZoom = desiredZoom;
+            this.currentPrio = prioAfter;
+        }
+    }
+
+
+}
