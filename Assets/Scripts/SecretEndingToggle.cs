dee50211642bbb88d67f30ed7959c02624a70606
D-Doge <tobias.niedermaier@googlemail.com>
2022-05-28 18:11:35 +0200

    Secert Edning Added

---
+++ Assets/Scripts/SecretEndingToggle.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretEndingToggle : MonoBehaviour
+{
+    public GameObject Collision;
+    public GameObject SecCollision;
+    public GameObject SecFoil;
+    public GameObject SecFolilNoLight;
+    public GameObject SecLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GameManager.Instance.getPesistentFeatherCount() >= 16)
+        {
+            Collision.SetActive(false);
+            SecCollision.SetActive(true);
+            SecFoil.SetActive(true);
+            SecFolilNoLight.SetActive(true);
+            SecLight.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
