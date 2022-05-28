4cb08738aca3dbaaaa80a99fe6e3c7bb2924e79b
D-Doge <tobias.niedermaier@googlemail.com>
2022-05-28 02:23:22 +0200

    FeatherCounter moves

--- Assets/Scripts/Level/FeatherCOunterUI.cs
+++ Assets/Scripts/Level/FeatherCOunterUI.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class FeatherCOunterUI : MonoBehaviour
 {
-    public string stringy= "Feathers: {0}/{1}";
+    public string stringy= "{0} of {1}";
     public int feathersInLevel = -1;
     public TMPro.TMP_Text text;
+
+    public RectTransform counter;
+
+    public float midTime = 1f;
+    private float midTimeCOunter;
+
+    private Vector3 startPos = new Vector3(718, -374, 0);
+    private Vector3 endPos = new Vector3(400, -225, 0);
+
+    public float startScale = 2f;
+    public float Tscale = 0.5f;
+    private float t = 0;
+
+    private Vector3 iAmLazy;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +28,20 @@ public class FeatherCOunterUI : MonoBehaviour
         {
             throw new System.Exception("TIMO");
         }
-
+        this.counter.anchoredPosition = startPos;
+        //this.iAmLazy = this.counter.localScale *= startScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         text.text = string.Format(this.stringy, GameManager.Instance.GetFeatherCountCurrentLevel(), this.feathersInLevel);
+        this.midTimeCOunter += Time.deltaTime;
+        if(this.midTime <= this.midTimeCOunter)
+        {
+            t += Time.deltaTime * this.Tscale;
+            this.counter.anchoredPosition = Vector3.Lerp(startPos, endPos, t);
+            // this.counter.localScale = Vector3.Lerp(iAmLazy, Vector3.one, t); Scaling fucks us
+        }
     }
 }
