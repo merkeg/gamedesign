5797ea534b699ea4efc4b0e81a89d42ade43f4be
D-Doge <tobias.niedermaier@googlemail.com>
2022-05-31 22:16:01 +0200

    Idk

---
+++ Assets/Scripts/Entity/Player/PlayerHit.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PlayerHit : MonoBehaviour
+{
+    public VolumeProfile VolumeProfile;
+    private Vignette _vignette;
+
+    public float vignetteSpeed = 5;
+    private bool hitVinette = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VolumeProfile.TryGet<Vignette>(out _vignette);
+        _vignette.intensity.overrideState = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            this.Hit();
+        }
+
+        if(this.hitVinette)
+        {
+            this._vignette.intensity.value += Time.deltaTime * this.vignetteSpeed;
+
+            if(this._vignette.intensity.value >= 1)
+            {
+                this.hitVinette = false;
+            }
+        }
+        else
+        {
+            this._vignette.intensity.value -= Time.deltaTime * this.vignetteSpeed;
+        }
+    }
+
+    public void Hit()
+    {
+        //Play Hit Sound
+
+        this.hitVinette = true;
+    }
+}
