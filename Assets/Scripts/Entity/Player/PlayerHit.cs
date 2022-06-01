d1c41d4c7c383d0a3a4e708e51540f23493b9760
D-Doge <tobias.niedermaier@googlemail.com>
2022-06-02 00:20:46 +0200

    PlayerHit

--- Assets/Scripts/Entity/Player/PlayerHit.cs
+++ Assets/Scripts/Entity/Player/PlayerHit.cs
@@ -6,20 +6,22 @@ using UnityEngine.Rendering.Universal;
 
 public class PlayerHit : MonoBehaviour
 {
-    public Transform Mask;
+    public RectTransform Mask;
     public GameObject Vin;
     private Vector3 startSize;
     private float t = 0;
     public float vignetteSpeed = 5;
     private bool hitVinette = false;
 
-    private Vector3 end = new Vector3(8, 8, 1);
+    private Vector3 end;
     private PlayerAudio playerAudio;
     // Start is called before the first frame update
     void Start()
     {
         this.playerAudio = this.transform.GetChild(9).GetComponent<PlayerAudio>();
         this.startSize = Mask.localScale;
+
+        this.end = startSize * 2;
     }
 
     // Update is called once per frame
@@ -53,7 +55,8 @@ public class PlayerHit : MonoBehaviour
         {
             this.Vin.SetActive(true);
         }
-        //this.Mask.localScale = Vector3.Lerp(startSize, end, t);
+        Debug.Log(t);
+        this.Mask.localScale = Vector3.Lerp(end, startSize, t);
     }
 
     public void Hit()
@@ -61,6 +64,7 @@ public class PlayerHit : MonoBehaviour
         //Play Hit Sound
         this.playerAudio.playDamage();
 
+        t = 1;
         this.hitVinette = true;
     }
 }
